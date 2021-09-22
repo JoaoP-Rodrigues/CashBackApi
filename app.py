6c0a5702bd5b56b36273ff780ae346aa3282af82
JoaoP-Rodrigues <joao.tribo@gmail.com>
2021-09-21 21:16:45 -0300

    Continue the development

--- app.py
+++ app.py
@@ -1,12 +1,62 @@
-from flask import Flask, Response, request
-from flask_sqlalchemy import SQLAlchemy
-import sqlite3
+from flask import Flask, request
 import json
-con = sqlite3.connect('users.db')
+from dbusers import insertUser
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = con
+def cpfValidate(cpfNotValidate):
 
-db = SQLAlchemy(app)
+    # take only numbers from cpf input
+    cpf = [int(char) for char in cpfNotValidate if char.isdigit()]
 
+    # verify if the CPF contain exactly 11 digits
+    if len(cpf) != 11:
+        return False
+
+    # test if the cpf contain a same number in 11 digits
+    # this will pass in validate
+    if cpf == cpf[::-1]:
+        return False
+
+    # verify the verifying digit
+    for i in range(9, 11):
+        value = sum((cpf[num] * ((i+1) - num) for num in range(0, i)))
+        digit = ((value * 10) % 11) % 10
+        if digit != cpf[i]:
+            return False
+    return True
+
+app = Flask("CashBack")
+
+@app.route("/api/cashback", methods=["POST"])
+def getCashBack():
+    body = request.get_json()
+    customer_cpf = body["customer"]["document"]
+
+    if not cpfValidate(customer_cpf):
+        response = {}
+        response["message"] = "CPF Inválido!"
+        return response
+
+    #customer_name = body["customer"]["name"]
+    #products = body["products"]
+
+    return
+
+'''@app.route("/register/user", methods=["POST"])
+def registerUser():
+
+    body = request.get_json()
+
+    user = insertUser(body["nome"], body["email"], body["senha"])
+
+    return createResponse(200, "User Created", "user", user)
+'''
+def createResponse(status, message, name_of_content=False, content=False):
+    response = {}
+    response["status"] = status
+    response["message"] = message
+
+    if(name_of_content and content):
+        response[name_of_content] = content
+    return response
+
+app.run()
--- dbusers.py
+++ dbusers.py
@@ -1,22 +1,61 @@
 import sqlite3
 import json
 import os
+
 os.remove("cashback.db") if os.path.exists("cashback.db") else None
 
 
-def insertUser(nome, email, senha):
-    con = sqlite3.connect('cashback.db')
-    sqlite3.Connection
+def createDB():
+    con = sqlite3.connect("cashback")
+    # sqlite3.Connection
     cur = con.cursor()
-    sqlite3.Cursor
-    sql_create = 'create table sells' \
-                 '(id integer primary key, ' \
-                 'customer_cpf varchar(11), ' \
-                 'prod_type varchar(20),' \
-                 'prod_value float(12)' \
-                 'prod_qty integer(100)'
+    # sqlite3.Cursor
+    sql_create_prod = 'create table products' \
+                      '(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, ' \
+                      'prod_type varchar(20),' \
+                      'prod_value float(12)' \
+                      'prod_cash integer(100))'
+    cur.execute(sql_create_prod)
 
-    cur.execute(sql_create)
+    sql_create_sells = 'create table sells' \
+                       '(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, ' \
+                       'sell_date date(20),' \
+                       'user_cpf varchar(20),' \
+                       'user_name varchar(100),' \
+                       'prod_type varchar(20),' \
+                       'prod_value float(12)' \
+                       'prod_qty float(12)' \
+                       'prod_cash integer(100))'
+
+    cur.execute(sql_create_sells)
 
 
+# def insertUser(nome, cpf, products, percentcash):
+#    con = sqlite3.connect('cashback.db')
+
+# this code must be run only time, for insert products in database
+# if you need add new products, update below datas and so execute the code.
+def insertProds():
+    con = sqlite3.connect("cashback")
+    cur = con.cursor()
+    sql_insert = 'insert into products values (?, ?, ?)'
+    rec_prods = [("Electronics", 1000, 5),
+                 ("Food", 100, 15),
+                 ("Games", 200, 12)]
+
+    for rec in rec_prods:
+        cur.execute(sql_insert, rec)
+
+
+def lecture():
+    conn = sqlite3.connect('cashback')
+    c = conn.cursor()
+    c.execute("SELECT * FROM PRODUTOS")
+    for linha in c.fetchall():
+        print(linha)
+
+
+createDB()
+insertProds()
+lecture()
 
