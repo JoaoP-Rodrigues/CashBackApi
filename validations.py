419afec63b2a821c79e8b0a5547ae7ae6c6ab54d
Joao Paulo Rodrigues <86681094+JoaoP-Rodrigues@users.noreply.github.com>
2021-09-23 12:02:42 -0300

    Created comments
    Comments add to facilitate the lecture of code

--- validations.py
+++ validations.py
@@ -26,6 +26,7 @@ def cpfValidate(cpfNotValidate):
 
 # this function will receive on input a product type
 # if the product exist in dict, will return the cashback percent, if not, return "0"
+# if necessary, you could add new category products, just add new elements in the dictionary 
 def checkProds(product):
     list_products = {"A": 15, "B": 5, "C": 10, "D": 12, "E": 3}
     if product not in list_products:
@@ -33,16 +34,19 @@ def checkProds(product):
     else:
         return list_products[product]
 
-
+# this function will validate the total value from sell
 def checkValues(totalSell, prods_values):
+    # take and convert the value from sell provided on input "json" to float format
     totalSell = float(totalSell)
     sumValues = 0.0
-    sumProds = {}
+    
+    # this for loop will take the quantity and value from all products
+    # so, it is increment this value in a sum
+    # next, compares sell's total and sum taked from values
     for p in prods_values:
         temp_sum = float(p["value"]) * p["qty"]
         sumValues += float(temp_sum)
         prod = p["type"]
-        sumProds[prod] = temp_sum
 
     if sumValues != totalSell:
         return False
@@ -54,7 +58,7 @@ def checkDate(date):
     today = datetime
     return today
 
-
+# simple function to get the sum values of all products in purchase
 def getSums(prod_value):
     sumProds = {}
     for v in prod_value:
@@ -64,11 +68,14 @@ def getSums(prod_value):
 
     return sumProds
 
-
+# this function will calculate the total of cashback
+# in input, it's get two dictionarys; first one have the percent of cashback from each product, and second one have the total'sum from values of each product. 
 def calculateCashbacks(dict_cashs, dict_sums):
     total_cash = 0
 
     for k, v in dict_sums.items():
+        # below, it's convert value of cashback to percent
+        # Ex.: 10 / 100 = 0.1 | Value Product = 50 | 50 * 0.1 = 5(10% from 50)
         convert_to_percent = dict_cashs[k] / 100
         temp_cash = v * convert_to_percent
         total_cash += temp_cash
